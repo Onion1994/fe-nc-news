12f80c8b6830d7186c19245d5a3d12edd32b3af4
Renato Cipolla <renato.cipolla94@gmail.com>
2023-12-12 09:43:29 +0000

    add isLoading

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react"
 import { getArticles } from "../api"
 
 export default function ArticleCard () {
+    const [isLoading, setIsLoading] = useState(true)
     const [articles, setArticles] = useState([])
     useEffect(() => {
         getArticles()
         .then((res) => {
             setArticles(res)
+            setIsLoading(false)
         })
     }, [])
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
 
     return articles.map((article) => {
         return <li className="component" key={article.article_id}><p>{article.title}</p><img className="article-img" src={article.article_img_url}></img></li>
