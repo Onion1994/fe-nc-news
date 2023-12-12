286719642cc3f3f7d9770cab5154e41ef511f059
Renato Cipolla <renato.cipolla94@gmail.com>
2023-12-12 18:58:37 +0000

    add upvote and downvote

--- components/CommentCard.jsx
+++ components/CommentCard.jsx
@@ -13,7 +13,7 @@ export default function CommentCard ({ article }) {
 
     return <ul className="component"><h2>Comments</h2>
     {currentComments.map((comment) => {
-        return <li className="component list stylised-box" key={comment.comment_id}>
+        return <li className="component stylised-box" key={comment.comment_id}>
             <h3>{comment.author}</h3>
             <p>{comment.body}</p>
             <p>{comment.created_at.slice(0, 10)} - {comment.created_at.slice(12, 16)}</p>
