fa875ea4059133065fffef8f8132b16d00f6a10a
Renato Cipolla <renato.cipolla94@gmail.com>
2023-12-14 12:07:07 +0000

    add comment deletion with error message

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,15 +1,16 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { UserContext } from "../contexts/userContext";
+import { UserContext } from "../contexts/UserContext";
 
-export default function Nav () {
+export default function Nav() {
+  const { user } = useContext(UserContext);
 
-const { user } = useContext(UserContext)
- 
-    return <nav className="component">
-        <Link to="/">
+  return (
+    <nav className="component">
+      <Link to="/">
         <p>Home</p>
-        </Link>
-        <p>Logged as: {user}</p>
+      </Link>
+      <p>Logged as: {user}</p>
     </nav>
+  );
 }
