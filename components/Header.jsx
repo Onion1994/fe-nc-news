e9e52e3575949e83c644bf78b25cdd967c454e5b
Renato Cipolla <renato.cipolla94@gmail.com>
2024-01-21 14:27:46 +0000

    add styling and logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,7 @@
+import logo from "../src/assets/North-Coders-News-logos_transparent.png"
+
 export default function Header () {
-    return <header>
-        <h1>NC News</h1>
+    return <header className="logo">
+       <img src={logo} alt='North Coders News (NCN) logo with a stylized design.'/>
     </header>
 }
